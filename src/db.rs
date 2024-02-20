d37630ba942fd3eb7de04b80fc965eb106a8df5c
Shamim Keshani <sh.keshani@gmail.com>
2024-02-20 13:09:18 +0330

    add database read/write functions

--- src/db.rs
+++ src/db.rs
@@ -1,14 +1,28 @@
+use std::path::Path;
+
 use redb::{Database, Error, ReadableTable, TableDefinition, RedbValue};
 
-const TABLE_U64_GOALS_NOTES: TableDefinition<i64, (Vec<&str>, Vec<&str>)> = TableDefinition::new("weeks");
-const db_file: &'static str = "~/.weeks_db";
+const TABLE_I64_GOALS_NOTES: TableDefinition<i64, (Vec<&str>, Vec<&str>)> = TableDefinition::new("weeks");
+const db_file: &'static str = "weeks_db.txt";
 
-pub fn read_week(ref_seconds: i64) -> Result<Option<(Vec<String>, Vec<String>)>, Error> {
+pub fn write_week(reference: i64, goals: Vec<String>, notes: Vec<String>) -> Result<(), Error> {
+    let db = Database::create(Path::new(db_file))?;
+    let txn = db.begin_write()?;
+    {
+        let mut table = txn.open_table(TABLE_I64_GOALS_NOTES)?;
+        let goals: Vec<&str> = goals.iter().map(String::as_str).collect();
+        let notes: Vec<&str> = notes.iter().map(String::as_str).collect();
+        table.insert(reference, (goals, notes))?;
+    }
+    txn.commit()?;
+    Ok(())
+}
 
-    let db = Database::create(db_file)?;
+pub fn read_week(reference: i64) -> Result<Option<(Vec<String>, Vec<String>)>, Error> {
+    let db = Database::create(Path::new(db_file))?;
     let txn = db.begin_read()?;
-    let table = txn.open_table(TABLE_U64_GOALS_NOTES)?;
-    let result = table.get(ref_seconds)?;
+    let table = txn.open_table(TABLE_I64_GOALS_NOTES)?;
+    let result = table.get(reference)?;
     if let Some(r) = result {
         let (goals, notes) = r.value();
         let goals: Vec<String> = goals.iter().map(|x| String::from(*x)).collect();
@@ -18,3 +32,4 @@ pub fn read_week(ref_seconds: i64) -> Result<Option<(Vec<String>, Vec<String>)>,
         Ok(None)
     }
 }
+
